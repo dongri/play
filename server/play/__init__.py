faf20c453bf59651e9cb0bd311bab39d402b164f
Dongri Jin <dongrify@gmail.com>
2017-11-05 00:06:20 +0900

    Fix: redis url

--- server/play/__init__.py
+++ server/play/__init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask_sse import sse
 
+
 app = Flask(__name__)
-app.config["REDIS_URL"] = "redis://localhost"
+if os.getenv("PLAY_ENV") == "docker":
+    app.config["REDIS_URL"] = "redis://play_redis"
+else:
+    app.config["REDIS_URL"] = "redis://localhost:"
 app.register_blueprint(sse, url_prefix='/stream')
 
 from play import views
